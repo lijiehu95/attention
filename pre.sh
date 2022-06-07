78538cd3f9e65172a53d83eb74a100d94128a55a
liuyixin-louis <zxcxzcz@qq.com>
2022-06-08 01:31:26 +0800

    data

--- pre.sh
+++ pre.sh
@@ -3,7 +3,7 @@ do
   if test -d $file
     echo $file is dir
     then
-        for file2 in $file
+        for file2 in $file/*
         do
           if [[ "$file2" == "pre.sh" ]];then
           echo "==/"a*/""
