ce6b8a17b34ad0723d850d499cae4081b39bf83c
liuyixin-louis <zxcxzcz@qq.com>
2022-06-08 01:30:22 +0800

    data

--- pre.sh
+++ pre.sh
@@ -1,10 +1,12 @@
 for file in ./preprocess/*
 do
-  if test -f $file
+  if test -d $file
     then
-        echo $file 是文件
-        if [[ "$file" == "pre.sh" ]];then
+        for file2 in $file
+        do
+          if [[ "$file2" == "pre.sh" ]];then
           echo "==/"a*/""
-        fi
+          fi
+        done
     fi
 done
