b5abdb8746ac9c7a1cd61fe0cd041b6ddf18f8c7
liuyixin-louis <zxcxzcz@qq.com>
2022-06-08 01:31:57 +0800

    data

--- pre.sh
+++ pre.sh
@@ -5,7 +5,7 @@ do
     then
         for file2 in $file/*
         do
-          if [[ "$file2" == "pre.sh" ]];then
+          if [[ "$file2" == "*pre.sh" ]];then
           echo "==/"a*/""
           fi
         done
