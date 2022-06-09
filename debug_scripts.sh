bbc8a35ac88ec8042b1020adbafea172a51d10e9
liuyixin-louis <zxcxzcz@qq.com>
2022-06-10 02:38:34 +0800

    data

--- debug_scripts.sh
+++ debug_scripts.sh
@@ -4,8 +4,8 @@ export PYTHONPATH=/home/yila22/prj
 exp_name="debug-and-find-lambda"
 
 for data in sst ; do
-  for lambda_1 in  1e-1 1e-2 1e-3 1e-4 1e-5;do
-    for lambda_2 in  1e-1 1e-2 1e-3 1e-4 1e-5;do
+  for lambda_1 in  1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7;do
+    for lambda_2 in  1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7;do
 python train_and_run_experiments_bc.py --dataset $data \
   --data_dir . --output_dir test_adv_outputs/ \
   --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 \
@@ -13,8 +13,8 @@ python train_and_run_experiments_bc.py --dataset $data \
 done;done;done
 
 for data in imdb ; do
-  for lambda_1 in  1e-1 1e-2 1e-3 1e-4 1e-5;do
-    for lambda_2 in  1e-1 1e-2 1e-3 1e-4 1e-5;do
+  for lambda_1 in  1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7;do
+    for lambda_2 in  1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7;do
 python train_and_run_experiments_bc.py --dataset $data \
   --data_dir . --output_dir test_adv_outputs/ \
   --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 \
