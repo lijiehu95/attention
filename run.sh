ee767adf75de19123b2c1b949440ef66456a9052
liuyixin-louis <zxcxzcz@qq.com>
2022-06-12 00:29:46 +0800

    data

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #export CUDA_VISIBLE_DEVICES=4
 source activate xai
 export PYTHONPATH=/home/yila22/prj
-exp_name="debug-topk-v1"
+exp_name="debug-topk-v2"
 i=0 # gpu pointer
-gpu=(4 5 6 7 9)
-gpunum=5
+gpu=(0 2 3 4 5 6 7 9)
+gpunum=8
 for data in sst; do
   for lambda_1 in 0;do
     for lambda_2 in  1 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7;do
@@ -19,7 +19,12 @@ for data in sst; do
             --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 --topk_prox_metric $topk_prox_metric --K $K\
             --gold_label_dir /home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Mon_Jun__6_13:13:30_2022/ --n_iters 80"
 #          $com
-          nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+          if [ $i == 7 ]
+          then
+            $com
+          else
+            nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+          fi
 done;done;done;done;done
 
 for data in imdb; do
@@ -36,7 +41,12 @@ for data in imdb; do
             --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 --topk_prox_metric $topk_prox_metric --K $K\
             --gold_label_dir /home/yila22/prj/attention/test_outputs/imdb/lstm+tanh/Thu_Jun__9_07:14:57_2022  --n_iters 80"
 #          $com
-          nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+          if [ $i == 7 ]
+          then
+            $com
+          else
+            nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+          fi
 done;done;done;done;done
 
 
