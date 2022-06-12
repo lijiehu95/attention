bcf8193831b3cd9dc0d8852bb8b9a2a6cd4c928f
liuyixin-louis <zxcxzcz@qq.com>
2022-06-12 12:43:59 +0800

    data

--- run.sh
+++ run.sh
@@ -18,7 +18,32 @@ for data in sst; do
             --data_dir . --output_dir test_adv_outputs/ \
             --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 --topk_prox_metric $topk_prox_metric --K $K\
             --gold_label_dir /home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Mon_Jun__6_13:13:30_2022/ --n_iters 80"
-          $com
+#          nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+#          $com
+          if [ $i == 7 ]
+          then
+            $com
+          else
+            nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+          fi
+          i=`expr $i + 1`
+done;done;done;done;done
+
+for data in imdb; do
+  for lambda_1 in 0;do
+    for lambda_2 in 1;do
+      for topk_prox_metric in l1 l2 kl-full jsd-full kl-topk jsd-topk;do
+        for K in 7;do
+          i=`expr $i % $gpunum`
+          export CUDA_VISIBLE_DEVICES=${gpu[$i]}
+          echo "use gpu id is ${gpu[$i]}"
+
+          com="python train.py --exp_name $exp_name --dataset $data \
+            --data_dir . --output_dir test_adv_outputs/ \
+            --encoder lstm --ours --lambda_1 $lambda_1 --lambda_2 $lambda_2 --topk_prox_metric $topk_prox_metric --K $K\
+            --gold_label_dir /home/yila22/prj/attention/test_outputs/imdb/lstm+tanh/Thu_Jun__9_07:14:57_2022/ --n_iters 80"
+#          nohup $com > ./logs/$data-${RANDOM}.txt 2>&1 &
+#          $com
           if [ $i == 7 ]
           then
             $com
