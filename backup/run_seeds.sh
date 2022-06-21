c561274c13add45de7f87851b287c83862c3445d
liuyixin-louis <zxcxzcz@qq.com>
2022-06-21 21:49:00 +0800

    data

--- backup/run_seeds.sh
+++
@@ -1,1 +0,0 @@
-python train_and_run_experiments_bc.py --dataset ${2} --data_dir . --output_dir outputs/seed_${1}/ --attention tanh --encoder lstm --seed ${1}
---
+++ run_seeds.sh
@@ -0,0 +1,12 @@
+
+source activate xai
+export PYTHONPATH=/home/yila22/prj
+export CUDA_VISIBLE_DEVICES='2,8'
+exp_name="debug-seed"
+
+
+for dataset in sst imdb hate emotion;do
+for seed in 50 257 500231 100078 12504 90754789 8988812 2; do
+python trian.py --dataset $dataset --data_dir . --output_dir test_outputs/dataset-${dataset}-seed-${seed}/ --attention tanh --encoder lstm --seed $seed \
+  --exp_name $exp_name
+done;done
