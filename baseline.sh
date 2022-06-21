ecea842d7e251ffe6e7980ef369a1f5387155ec5
liuyixin-louis <zxcxzcz@qq.com>
2022-06-21 21:30:30 +0800

    data

--- baseline.sh
+++ baseline.sh
@@ -2,4 +2,7 @@ source activate xai
 export PYTHONPATH=/home/yila22/prj
 export CUDA_VISIBLE_DEVICES='2,8'
 exp_name="debug-baseline"
-python trian.py --dataset ${1} --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm
+python trian.py --dataset sst --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm
+python trian.py --dataset imdb --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm
+python trian.py --dataset hate --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm
+python trian.py --dataset emotion --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm
