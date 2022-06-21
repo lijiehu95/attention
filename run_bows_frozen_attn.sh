6a6409083c88d6fb70111f861979ff906d13c4c7
liuyixin-louis <zxcxzcz@qq.com>
2022-06-21 21:53:22 +0800

    data

---
+++ run_bows_frozen_attn.sh
@@ -0,0 +1,12 @@
+
+
+source activate xai
+export PYTHONPATH=/home/yila22/prj
+export CUDA_VISIBLE_DEVICES='2,8'
+exp_name="debug-freeze-att"
+
+
+for dataset in sst imdb hate emotion;do
+python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention frozen --encoder average  \
+  --exp_name $exp_name
+done;
