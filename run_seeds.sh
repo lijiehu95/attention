f01b1fa190aa9976b13271b5ee12f38a6729a693
liuyixin-louis <zxcxzcz@qq.com>
2022-06-21 21:50:40 +0800

    data

--- run_seeds.sh
+++ run_seeds.sh
@@ -7,6 +7,6 @@ exp_name="debug-seed"
 
 for dataset in sst imdb hate emotion;do
 for seed in 50 257 500231 100078 12504 90754789 8988812 2; do
-python trian.py --dataset $dataset --data_dir . --output_dir test_outputs/dataset-${dataset}-seed-${seed}/ --attention tanh --encoder lstm --seed $seed \
+python train.py --dataset $dataset --data_dir . --output_dir test_outputs/dataset-${dataset}-seed-${seed}/ --attention tanh --encoder lstm --seed $seed \
   --exp_name $exp_name
 done;done
