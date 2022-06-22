05925743104befc5df671530727d72435b2a4c2f
liuyixin-louis <zxcxzcz@qq.com>
2022-06-23 00:19:48 +0800

    data

--- run_bows_frozen_attn.sh
+++ run_bows_frozen_attn.sh
@@ -8,6 +8,7 @@ exp_name="freeze-att-average-v1"
 
 #for dataset in sst imdb hate emotion;do
 for dataset in hate offensive rotten_tomatoes;do
-python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention frozen --encoder average  \
-  --exp_name $exp_name
+  com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention frozen --encoder average  \
+    --exp_name $exp_name"
+  nohup $com > $exp_name-$dataset.log 2>&1 &
 done;
