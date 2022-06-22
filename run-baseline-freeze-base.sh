0a4f1abbe014100629079e2ac57344d59270c9b8
liuyixin-louis <zxcxzcz@qq.com>
2022-06-22 23:48:11 +0800

    data

--- run-baseline-freeze-base.sh
+++ run-baseline-freeze-base.sh
@@ -1,19 +1,19 @@
 source activate xai
 export PYTHONPATH=/home/yila22/prj
 export CUDA_VISIBLE_DEVICES='9'
-exp_name="baseline-final-v3"
+exp_name="baseline-final"
 
 n_iters=80
 #datasets=("offensive" "irony" "rotten_tomatoes")
-
-for dataset in  imdb sst hate offensive rotten_tomatoes irony ; do
+# imdb sst hate offensive rotten_tomatoes irony
+for dataset in hate  ; do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --exp_name $exp_name --n_iters $n_iters"
 #  $com
   nohup $com > $exp_name-$dataset.log 2>&1 &
 done;
 export CUDA_VISIBLE_DEVICES='4'
-exp_name="freeze-att-lstm-final-v3"
-for dataset in  imdb sst hate offensive rotten_tomatoes irony;do
+exp_name="freeze-att-lstm-final"
+for dataset in  hate;do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention frozen --encoder lstm  \
     --exp_name $exp_name --n_iters $n_iters"
 #  $com
