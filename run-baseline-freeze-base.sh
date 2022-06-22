cd64c00701a0c0c6a871cf7265088b9e65a8896b
liuyixin-louis <zxcxzcz@qq.com>
2022-06-22 23:01:05 +0800

    data

--- run-baseline-freeze-base.sh
+++ run-baseline-freeze-base.sh
@@ -6,14 +6,14 @@ exp_name="baseline-final"
 n_iters=80
 #datasets=("offensive" "irony" "rotten_tomatoes")
 
-for dataset in  "irony" "rotten_tomatoes"; do
+for dataset in  offensive imdb sst emotion; do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --exp_name $exp_name --n_iters $n_iters"
 #  $com
   nohup $com > $exp_name-$dataset.log 2>&1 &
 done;
 export CUDA_VISIBLE_DEVICES='4'
 exp_name="freeze-att-lstm-final"
-for dataset in  "irony" "rotten_tomatoes";do
+for dataset in  offensive imdb sst emotion;do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention frozen --encoder lstm  \
     --exp_name $exp_name --n_iters $n_iters"
 #  $com
