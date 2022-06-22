a5771d50f335d9557ba23420e19dfb40b38b965c
liuyixin-louis <zxcxzcz@qq.com>
2022-06-23 00:18:45 +0800

    data

--- run_bows_baselines.sh
+++ run_bows_baselines.sh
@@ -9,8 +9,9 @@ exp_name="freeze-bows-with-train-att-v1"
 #for dataset in sst imdb hate emotion;do
 
 for dataset in hate offensive rotten_tomatoes;do
-  python train.py --dataset ${dataset} --data_dir . --output_dir test_outputs/ --attention tanh --encoder average \
-    --exp_name $exp_name
+  com="python train.py --dataset ${dataset} --data_dir . --output_dir test_outputs/ --attention tanh --encoder average \
+    --exp_name $exp_name"
+  nohup $com > $exp_name-$dataset.log 2>&1 &
 done;
 
 
