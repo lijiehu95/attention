3b3a6b81f05e8f8fb02fa36cdd1807b8764eeb28
liuyixin-louis <zxcxzcz@qq.com>
2022-06-23 01:59:05 +0800

    data

--- run_adversarial_ours_debug.sh
+++ run_adversarial_ours_debug.sh
@@ -15,13 +15,14 @@ golddir=("/home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Wed_Jun_22_11:01
 
 n_iters=40
 K=7
-for pgd_radius in 0.01 0.05 0.1 0.2 0.3;do
-for x_pgd_radius in 0.001 0.01 0.05 0.1 0.2 0.3; do
+for pgd_radius in 0.01 0.05 0.1;do
+for x_pgd_radius in 0.01 0.05 0.1; do
 for datasetid in 0; do
 for lambda_1 in 0 1; do
   for lambda_2 in 0 1; do
-    python train.py --dataset ${dataset[$datasetid]} --data_dir . --output_dir test_adv_outputs/ \
+    com="python train.py --dataset ${dataset[$datasetid]} --data_dir . --output_dir test_adv_outputs/ \
       --encoder lstm --ours --gold_label_dir ${golddir[$datasetid]} --n_iters $n_iters \
         --exp_name $exp_name --lambda_1 $lambda_1 --lambda_2 $lambda_2 --pgd_radius $pgd_radius --x_pgd_radius $x_pgd_radius \
-        --K $K
+        --K $K"
+    nohup $com > ./logs/$exp_name-$RANDOM.log 2>&1 &
 done;done;done;done;done;
