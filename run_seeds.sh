b6ffea427ed91019eb5d6db869678191f293d77a
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 04:11:00 +0800

    first

--- run_seeds.sh
+++ run_seeds.sh
@@ -7,7 +7,7 @@ n_iters=40
 for model in simple-rnn; do
 for dataset in sst imdb hate rotten_tomatoes;do
 for seed in 50 257 500231 2; do
- com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder $model --seed $seed \
+ com="python train.py --dataset $dataset --data_dir . --output_dir seed_output/ --attention tanh --encoder $model --seed $seed \
   --exp_name $exp_name --n_iters $n_iters"
   nohup $com > $exp_name-$dataset-$seed.log 2>&1 &
 #  $com
