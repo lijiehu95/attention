4e57bb078b93b55adaa832a6f7463812727da867
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 02:53:23 +0800

    first

--- collect.py
+++ collect.py
@@ -1,14 +1,14 @@
 from glob import glob
 
-dataset = "hate"
-path = f"/home/yila22/prj/attention/test_outputs/{dataset}/average*/*"
-import os
-print(dataset)
-for sd in glob(path):
-    import json
-    print(sd)
-    with open(os.path.join(sd,'evaluate.json')) as f:
-        d = json.load(
-            f
-        )
-        print(d['macro avg/f1-score'])
+for dataset in ['imdb', 'sst', 'hate', 'rotten_tomatoes']:
+    path = f"/home/yila22/prj/attention/test_outputs/{dataset}/average*/*"
+    import os
+    print(dataset)
+    for sd in glob(path):
+        import json
+        print(sd)
+        with open(os.path.join(sd,'evaluate.json')) as f:
+            d = json.load(
+                f
+            )
+            print(d['macro avg/f1-score'])
