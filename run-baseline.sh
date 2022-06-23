031041ffd305088d6c28734405995104e91ec13e
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 03:52:02 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -2,6 +2,6 @@ source activate xai
 export PYTHONPATH=/home/yila22/prj
 export CUDA_VISIBLE_DEVICES='9'
 exp_name="baseline-v1"
-for dataset in sst imdb hate emotion; do
+for dataset in sst imdb hate rotten_tomatoes; do
 python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --exp_name $exp_name
 done;
