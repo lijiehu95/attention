f43a98a6e52912b4b5f4c543ba62e0f5e5fa90d2
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 04:11:29 +0800

    first

--- run_seeds.sh
+++ run_seeds.sh
@@ -9,6 +9,6 @@ for dataset in sst imdb hate rotten_tomatoes;do
 for seed in 50 257 500231 2; do
  com="python train.py --dataset $dataset --data_dir . --output_dir seed_output/ --attention tanh --encoder $model --seed $seed \
   --exp_name $exp_name --n_iters $n_iters"
-  nohup $com > $exp_name-$dataset-$seed.log 2>&1 &
-#  $com
+#  nohup $com > $exp_name-$dataset-$seed.log 2>&1 &
+  $com
 done;done;done
