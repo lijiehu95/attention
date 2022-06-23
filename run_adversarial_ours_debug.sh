ae429e8645a940ec2bc70be6dfc7bfd98d49d1fb
liuyixin-louis <zxcxzcz@qq.com>
2022-06-23 11:54:41 +0800

    data

--- run_adversarial_ours_debug.sh
+++ run_adversarial_ours_debug.sh
@@ -11,19 +11,19 @@ golddir=( "/home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Wed_Jun_22_11:0
   "/home/yila22/prj/attention/test_outputs/hate/lstm+tanh/Tue_Jun_21_15:33:19_2022/" \
   "/home/yila22/prj/attention/test_outputs/offensive/lstm+tanh/Wed_Jun_22_11:01:36_2022/" \
   "/home/yila22/prj/attention/test_outputs/rotten_tomatoes/lstm+tanh/Wed_Jun_22_11:00:16_2022/" \
-  )
+)
 
-gpu=(2 3 4 5 6 7 8)
-gpunum=7
+gpu=(0 1 2 3)
+gpunum=4
 i=0 # gpu pointer
 
 n_iters=40
 K=7
-for pgd_radius in 0.01;do
-for x_pgd_radius in 0.001; do
+for pgd_radius in 0.001;do
+for x_pgd_radius in 0.0005; do
 for datasetid in 0; do
 for lambda_1 in 1; do
-  for lambda_2 in 1e-2 5e-2 1e-1 2e-1 5e-1 7e-1; do
+  for lambda_2 in 1e-3 1e-2 5e-2 1e-1 2e-1 5e-1 7e-1; do
     # in the for loop
    i=`expr $i % $gpunum`
    export CUDA_VISIBLE_DEVICES=${gpu[$i]}
