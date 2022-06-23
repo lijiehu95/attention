a05acc2ee1ed7d88d158aaea3222119639bbeef3
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 02:51:45 +0800

    first

---
+++ collect.py
@@ -0,0 +1,13 @@
+from glob import glob
+
+dataset = "hate"
+path = f"/home/yila22/prj/attention/test_outputs/{dataset}/average*/*"
+import os
+print(dataset)
+for sd in glob(path):
+    import json
+    with open(os.path.join(sd,'evaluate.json')) as f:
+        d = json.load(
+            f
+        )
+        print(d['macro avg/f1-score'])
