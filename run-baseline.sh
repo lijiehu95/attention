891d01a9003f2fa8fa2b69af37ac4c5bbb8db42d
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 03:52:30 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -3,5 +3,6 @@ export PYTHONPATH=/home/yila22/prj
 export CUDA_VISIBLE_DEVICES='9'
 exp_name="baseline-v1"
 for dataset in sst imdb hate rotten_tomatoes; do
-python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --exp_name $exp_name
+python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
+  --encoder simple-rnn --exp_name $exp_name
 done;
