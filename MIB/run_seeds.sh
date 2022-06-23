90ebafe906e3ba0bce95871be7a8df8ba5e0a904
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 01:28:08 +0800

    first

--- MIB/run_seeds.sh
+++ MIB/run_seeds.sh
@@ -1,13 +1,13 @@
 
 source activate xai
-export PYTHONPATH=/home/yila22/prj
+export PYTHONPATH=/mnt/yixin/
 export CUDA_VISIBLE_DEVICES='0'
-exp_name="seed-v1"
+exp_name="seed-v2"
 
 n_iters=40
 for dataset in sst imdb hate rotten_tomatoes;do
-for seed in 50 257 500231 2; do
- com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --seed $seed \
+for seed in 50 257 ; do
+  com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh --encoder lstm --seed $seed \
   --exp_name $exp_name --n_iters $n_iters"
   nohup $com > $exp_name-$dataset-$seed.log 2>&1 &
 done;done
