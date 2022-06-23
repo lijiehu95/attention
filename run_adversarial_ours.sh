457d053b356e40c71ade6be9d73fcaa6dec92259
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 03:23:33 +0800

    first

--- run_adversarial_ours.sh
+++ run_adversarial_ours.sh
@@ -16,10 +16,10 @@ golddir=( "/home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Wed_Jun_22_11:0
 n_iters=40
 K=7
 for pgd_radius in 0.01;do
-for x_pgd_radius in 0.001 0.01; do
+for x_pgd_radius in 0.001; do
 for datasetid in 0 1 2 3 4 ; do
-for lambda_1 in 0 1e-1 1; do
-  for lambda_2 in 0 1e-1 1; do
+for lambda_1 in 1; do
+  for lambda_2 in 1; do
     com="python train.py --dataset ${dataset[$datasetid]} --data_dir . --output_dir test_adv_outputs/ \
       --encoder lstm --ours --gold_label_dir ${golddir[$datasetid]} --n_iters $n_iters \
         --exp_name $exp_name --lambda_1 $lambda_1 --lambda_2 $lambda_2 --pgd_radius $pgd_radius --x_pgd_radius $x_pgd_radius \
