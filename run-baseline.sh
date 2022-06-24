26da4022c2e4f1e22ad9555ff4ba84bf49010242
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 14:25:37 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -3,7 +3,7 @@ export PYTHONPATH=${PYTHONPATH}:/home/yila22/prj:/mnt/yixin/
 export CUDA_VISIBLE_DEVICES='0'
 exp_name="baseline"
 for model in simple-rnn lstm; do
-for dataset in sst imdb hate rotten_tomatoes; do
+for dataset in sst imdb ; do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
     --encoder $model --exp_name $exp_name"
   nohup $com > $exp_name-$RANDOM.log 2>&1 &
