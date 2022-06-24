e6afdcbd7987bf99a844aa65f15870d4a1992752
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 13:19:40 +0800

    first

--- Trainers/TrainerBC.py
+++ Trainers/TrainerBC.py
@@ -103,7 +103,7 @@ class Trainer() :
         # wandb.log({
         #     "original_test_metrics":test_metrics
         # })
-
+        print("training our model")
         for i in tqdm(range(args.n_iters)):
 
             loss_tr, loss_tr_orig, tvd_loss_tr, topk_loss_tr, pgd_tvd_loss_tr,true_topk_loss_tr = self.model.train_ours(train_data.X, train_data.y,
@@ -234,69 +234,69 @@ class Trainer() :
                 break
 
         # self.model.related_score(test_data.X,test_data.y,test_data.true_pred,test_data.gold_attns,train=False,preturb_x=True,X_PGDer=self.X_PGDer)
-
-    def train_adversarial(self, train_data, test_data, args) :
-
-        br = False
-        n_fail = 0
-        best_loss = 10000000000
-        for i in tqdm(range(args.n_iters)) :
-
-            _, loss_tr, loss_tr_orig, tvd_loss_tr, kl_loss_tr = self.model.train(train_data.X, train_data.y, train_data.true_pred, train_data.gold_attns)
-            predictions_tr, attentions_tr, jsd_score_tr = self.model.evaluate(train_data.X, target_attn=train_data.gold_attns)
-            predictions_tr = np.array(predictions_tr)
-            train_metrics = self.metrics(np.array(train_data.y), predictions_tr, np.array(train_data.true_pred), jsd_score_tr)
-            print_str = "FULL (WEIGHTED) LOSS: %f | ORIG (UNWEIGHTED) LOSS: %f | KL: %f | TVD: %f" % (loss_tr, loss_tr_orig, kl_loss_tr, tvd_loss_tr)
-            print(print_str)
-
-            print("TRAIN METRICS:")
-            if self.display_metrics:
-                print_metrics(train_metrics, adv=True)
-
-            predictions_te, attentions_te, jsd_score_te = self.model.evaluate(test_data.X, target_attn=test_data.gold_attns)
-            predictions_te = np.array(predictions_te)
-            test_metrics = self.metrics(np.array(test_data.y), predictions_te, np.array(test_data.true_pred), jsd_score_te)
-
-            print("TEST METRICS:")
-            if self.display_metrics:
-                print_metrics(test_metrics, adv=True)
-
-            if loss_tr < best_loss:
-                best_loss = loss_tr
-                n_fail = 0
-                save_model = True
-                print("Model Saved on Training Loss: ", loss_tr)
-
-            else :
-                n_fail += 1
-                save_model = False
-                print("Model not saved on Training Loss: ", loss_tr)
-                if n_fail >= 10:
-                    br = True
-                    print("Loss hasn't decreased for 10 epochs...EARLY STOPPING TRIGGERED")
-                    
-            dirname = self.model.save_values(save_model=save_model)
-            if save_model:
-                attentions_tr = [el.tolist() for el in attentions_tr]
-                attentions_te = [el.tolist() for el in attentions_te]
-                print("SAVING PREDICTIONS AND ATTENTIONS")
-                json.dump(predictions_tr.tolist(), codecs.open(dirname + '/train_predictions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-                json.dump(predictions_te.tolist(), codecs.open(dirname + '/test_predictions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-                json.dump(attentions_tr, codecs.open(dirname + '/train_attentions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-                json.dump(attentions_te, codecs.open(dirname + '/test_attentions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
-
-            print("DIRECTORY:", dirname)
-
-            f = open(dirname + '/epoch.txt', 'a')
-            f.write(str(test_metrics) + '\n')
-            f.close()
-
-            f = open(dirname + '/losses.txt', 'a')
-            f.write("EPOCH %d: " % i + print_str + '\n')
-            f.close()
-
-            if br:
-                break
+    #
+    # def train_adversarial(self, train_data, test_data, args) :
+    #
+    #     br = False
+    #     n_fail = 0
+    #     best_loss = 10000000000
+    #     for i in tqdm(range(args.n_iters)) :
+    #
+    #         _, loss_tr, loss_tr_orig, tvd_loss_tr, kl_loss_tr = self.model.train(train_data.X, train_data.y, train_data.true_pred, train_data.gold_attns)
+    #         predictions_tr, attentions_tr, jsd_score_tr = self.model.evaluate(train_data.X, target_attn=train_data.gold_attns)
+    #         predictions_tr = np.array(predictions_tr)
+    #         train_metrics = self.metrics(np.array(train_data.y), predictions_tr, np.array(train_data.true_pred), jsd_score_tr)
+    #         print_str = "FULL (WEIGHTED) LOSS: %f | ORIG (UNWEIGHTED) LOSS: %f | KL: %f | TVD: %f" % (loss_tr, loss_tr_orig, kl_loss_tr, tvd_loss_tr)
+    #         print(print_str)
+    #
+    #         print("TRAIN METRICS:")
+    #         if self.display_metrics:
+    #             print_metrics(train_metrics, adv=True)
+    #
+    #         predictions_te, attentions_te, jsd_score_te = self.model.evaluate(test_data.X, target_attn=test_data.gold_attns)
+    #         predictions_te = np.array(predictions_te)
+    #         test_metrics = self.metrics(np.array(test_data.y), predictions_te, np.array(test_data.true_pred), jsd_score_te)
+    #
+    #         print("TEST METRICS:")
+    #         if self.display_metrics:
+    #             print_metrics(test_metrics, adv=True)
+    #
+    #         if loss_tr < best_loss:
+    #             best_loss = loss_tr
+    #             n_fail = 0
+    #             save_model = True
+    #             print("Model Saved on Training Loss: ", loss_tr)
+    #
+    #         else :
+    #             n_fail += 1
+    #             save_model = False
+    #             print("Model not saved on Training Loss: ", loss_tr)
+    #             if n_fail >= 10:
+    #                 br = True
+    #                 print("Loss hasn't decreased for 10 epochs...EARLY STOPPING TRIGGERED")
+    #
+    #         dirname = self.model.save_values(save_model=save_model)
+    #         if save_model:
+    #             attentions_tr = [el.tolist() for el in attentions_tr]
+    #             attentions_te = [el.tolist() for el in attentions_te]
+    #             print("SAVING PREDICTIONS AND ATTENTIONS")
+    #             json.dump(predictions_tr.tolist(), codecs.open(dirname + '/train_predictions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+    #             json.dump(predictions_te.tolist(), codecs.open(dirname + '/test_predictions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+    #             json.dump(attentions_tr, codecs.open(dirname + '/train_attentions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+    #             json.dump(attentions_te, codecs.open(dirname + '/test_attentions_best_epoch.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+    #
+    #         print("DIRECTORY:", dirname)
+    #
+    #         f = open(dirname + '/epoch.txt', 'a')
+    #         f.write(str(test_metrics) + '\n')
+    #         f.close()
+    #
+    #         f = open(dirname + '/losses.txt', 'a')
+    #         f.write("EPOCH %d: " % i + print_str + '\n')
+    #         f.close()
+    #
+    #         if br:
+    #             break
 
 class Evaluator() :
     def __init__(self, dataset, dirname, args) :
