90643589c3ff864868186a6ccdc5d1d1376a462c
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 14:25:51 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -6,5 +6,5 @@ for model in simple-rnn lstm; do
 for dataset in sst imdb ; do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
     --encoder $model --exp_name $exp_name"
-  nohup $com > $exp_name-$RANDOM.log 2>&1 &
+  nohup $com > $exp_name-$model-$dataset-$RANDOM.log 2>&1 &
 done;done;
