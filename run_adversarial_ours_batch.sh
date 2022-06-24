6f2d3327fad598e47cf97831c9d3c8bd25d5811e
liuyixin-louis <zxcxzcz@qq.com>
2022-06-24 15:36:44 +0800

    first

--- run_adversarial_ours_batch.sh
+++ run_adversarial_ours_batch.sh
@@ -6,11 +6,11 @@ export PYTHONPATH=${PYTHONPATH}:/home/yila22/prj:/mnt/yixin/
 exp_name="ours-final-624-v1"
 
 dataset=(sst imdb hate rotten_tomatoes)
-golddir=( "/home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Wed_Jun_22_11:01:35_2022/" \
-  "/home/yila22/prj/attention/test_outputs/imdb/lstm+tanh/Wed_Jun_22_11:01:34_2022/" \
-  "/home/yila22/prj/attention/test_outputs/hate/lstm+tanh/Tue_Jun_21_15:33:19_2022/" \
-  "/home/yila22/prj/attention/test_outputs/rotten_tomatoes/lstm+tanh/Wed_Jun_22_11:00:16_2022/" \
-)
+#golddir=( "/home/yila22/prj/attention/test_outputs/sst/lstm+tanh/Wed_Jun_22_11:01:35_2022/" \
+#  "/home/yila22/prj/attention/test_outputs/imdb/lstm+tanh/Wed_Jun_22_11:01:34_2022/" \
+#  "/home/yila22/prj/attention/test_outputs/hate/lstm+tanh/Tue_Jun_21_15:33:19_2022/" \
+#  "/home/yila22/prj/attention/test_outputs/rotten_tomatoes/lstm+tanh/Wed_Jun_22_11:00:16_2022/" \
+#)
 
 gpu=(0 1 2 3 4 5 6 7 8 9)
 gpunum=10
@@ -18,6 +18,7 @@ i=0 # gpu pointer
 j=0
 n_iters=40
 K=7
+for model in lstm simple-rnn; do
 for pgd_radius in 0.001;do
 for x_pgd_radius in 0.005; do
 for datasetid in 1; do
@@ -26,8 +27,8 @@ for lambda_1 in 1; do
    i=`expr $i % $gpunum`
    export CUDA_VISIBLE_DEVICES=${gpu[$i]}
    echo "use gpu id is ${gpu[$i]}"
-   com="python train.py --dataset ${dataset[$datasetid]} --data_dir . --output_dir test_adv_outputs/ \
-    --encoder lstm --ours --gold_label_dir ${golddir[$datasetid]} --n_iters $n_iters \
+   com="python train.py --dataset ${dataset[$datasetid]} --data_dir . --output_dir test_ours_outputs/ \
+    --encoder $model --ours --n_iters $n_iters \
       --exp_name $exp_name --lambda_1 $lambda_1 --lambda_2 $lambda_2 --pgd_radius $pgd_radius --x_pgd_radius $x_pgd_radius \
       --K $K"
 #    nohup $com > ./logs/$exp_name-$RANDOM.log 2>&1 &
@@ -39,6 +40,7 @@ for lambda_1 in 1; do
 #      sleep 20m
 #    fi
   done;
+done;
 done;
 done;
 done;
