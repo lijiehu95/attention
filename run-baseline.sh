d419e89fe1846bb110ceacf7ba65bf9e04078c1e
liuyixin-louis <zxcxzcz@qq.com>
2022-06-28 11:57:12 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -3,7 +3,8 @@ export PYTHONPATH=${PYTHONPATH}:/home/yila22/prj:/mnt/yixin/
 export CUDA_VISIBLE_DEVICES='0'
 exp_name="baseline"
 for model in simple-rnn lstm; do
-for dataset in sst imdb ; do
+for dataset in emoji sentiment stance_abortion stance_atheism stance_climate stance_feminist \
+                stance_hillary ; do
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
     --encoder $model --exp_name $exp_name"
   nohup $com > $exp_name-$model-$dataset-$RANDOM.log 2>&1 &
