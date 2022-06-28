5652afc89a6574c9b57eb8f1d93b87ed95018c46
liuyixin-louis <zxcxzcz@qq.com>
2022-06-28 12:08:24 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -17,7 +17,7 @@ for dataset in emoji sentiment stance_abortion stance_atheism stance_climate sta
    # your command here
 
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
-    --encoder $model --exp_name $exp_name"
+    --encoder $model --exp_name $exp_name --baseline"
 #  nohup $com > $exp_name-$model-$dataset-$RANDOM.log 2>&1 &
 $com
    i=`expr $i + 1`
