57242814a522d7e605000913429fc9791453ee47
liuyixin-louis <zxcxzcz@qq.com>
2022-06-28 11:59:48 +0800

    first

--- run-baseline.sh
+++ run-baseline.sh
@@ -15,9 +15,11 @@ for dataset in emoji sentiment stance_abortion stance_atheism stance_climate sta
    echo "use gpu id is ${gpu[$i]}"
 
    # your command here
+
   com="python train.py --dataset $dataset --data_dir . --output_dir test_outputs/ --attention tanh \
     --encoder $model --exp_name $exp_name"
-  nohup $com > $exp_name-$model-$dataset-$RANDOM.log 2>&1 &
+#  nohup $com > $exp_name-$model-$dataset-$RANDOM.log 2>&1 &
+$com
    i=`expr $i + 1`
 
 done;done;
