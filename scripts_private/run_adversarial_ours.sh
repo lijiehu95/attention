a245e72d8c1c6ab77dd19e6d1fc0d77182a3139c
liuyixin-louis <zxcxzcz@qq.com>
2022-06-30 10:21:34 +0800

    uploadg

--- scripts_private/run_adversarial_ours.sh
+++ scripts_private/run_adversarial_ours.sh
@@ -15,8 +15,8 @@ n_iters=40
 K=7
 
 for model in lstm; do
-for pgd_radius in 0.001 0.005 0.01 0.05 0.1;do
-for x_pgd_radius in 0.001 0.005 0.01 0.05 0.1; do
+for pgd_radius in 0.001;do
+for x_pgd_radius in 0.001 ; do
 for datasetid in 1; do
 for lambda_1 in 0 1e-3 1e-2 1e-1 1; do
   for lambda_2 in 0 1e-3 1e-2 1e-1 1; do
