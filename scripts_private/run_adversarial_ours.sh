559a0f7e906f16d797ebd206577fcfbc5769a0b7
liuyixin-louis <zxcxzcz@qq.com>
2022-06-30 10:33:38 +0800

    uploadg

--- scripts_private/run_adversarial_ours.sh
+++ scripts_private/run_adversarial_ours.sh
@@ -7,7 +7,7 @@ exp_name="find-best-hyperparameters-v2"
 dataset=(hate rotten_tomatoes  imdb sst emoji  \
                 sentiment  stance_abortion  stance_atheism  stance_climate  stance_feminist  \
                 stance_hillary)
-gpu=(2 3 4 5 6 7 8 9)
+gpu=(2 4 5 6 7 8 9)
 gpunum=${#gpu[@]}
 i=0 # gpu pointer
 j=0
