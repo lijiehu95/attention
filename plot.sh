e4ba082b3f38830d1377df64154e3ee86c1880cb
Yixin Liu <53036760+liuyixin-louis@users.noreply.github.com>
2022-07-03 20:09:28 +0800

    upload

--- plot.sh
+++ plot.sh
@@ -1,5 +1,7 @@
-for dataset in hate rotten_tomatoes  imdb sst emoji;do
+for dataset in hate rotten_tomatoes  imdb sst emoji  \
+                sentiment  stance_abortion  stance_atheism  stance_climate  stance_feminist  \
+                stance_hillary;do
 for att in tanh;do
-for model_type in simple-rnn; do
+for model_type in lstm simple-rnn; do
 python seed_graphs.py --dataset $dataset --model_type $model_type+$att
 done; done; done;
