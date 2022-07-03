b84109ba3e49b0fd907c08f6b1711a0d7d2c0a5e
liuyixin-louis <zxcxzcz@qq.com>
2022-07-03 16:48:00 +0800

    upload

--- plot_seed.sh
+++ plot_seed.sh
@@ -1,7 +1,7 @@
 for dataset in hate rotten_tomatoes  imdb sst emoji  \
                 sentiment  stance_abortion  stance_atheism  stance_climate  stance_feminist  \
                 stance_hillary;do
-for att in tanh;do
+for att in ours;do
 for model_type in lstm simple-rnn; do
 python seed_graphs.py --dataset $dataset --model_type $model_type+$att
 done; done; done;
